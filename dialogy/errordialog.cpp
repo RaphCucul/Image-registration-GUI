2cd6938c698599b4c3d98910b586e75924051de9
Jan Prosser <k.o.jouda@seznam.cz>
2018-12-31 22:28:16 +0100

    ErrorDialog changes
    Error dialog reimplemented with new features - no window

--- dialogy/errordialog.cpp
+++ dialogy/errordialog.cpp
@@ -4,18 +4,125 @@
 #include <QtGui>
 
 ErrorDialog::ErrorDialog(QWidget *parent) :
-    QDialog(parent),
-    ui(new Ui::ErrorDialog)
+    QObject(parent)
 {
-    ui->setupUi(this);
+
 }
 
 ErrorDialog::~ErrorDialog()
 {
-    delete ui;
+
 }
 
-void ErrorDialog::on_pushButton_clicked()
+void ErrorDialog::initialiseErrorLabel()
 {
+    /*errorLabelEffect = new QGraphicsColorizeEffect();
+    errorLabelEffect->setColor(Qt::white);
+    errorLabelEffect->setStrength(0.3);
+    errorLabelEffect->setEnabled(false);*/
+    errorLabel = new QLabel();
+    errorLabel->setMargin(0);
+    //errorLabel->installEventFilter(this);
+   // errorLabel->setGraphicsEffect(errorLabelEffect);
+
+    errorLabelParent = new QWidget(parentOfTheWidget);
+    errorLabelParent->setParent(parentOfTheWidget);
+    errorLabelParent->setLayout(new QHBoxLayout());
+    errorLabelParent->layout()->setMargin(0);
+    errorLabelParent->layout()->addWidget(errorLabel);
+    errorLabelParent->setVisible(true);
+}
+
+void ErrorDialog::analyseParents(QWidget *widgetWithLabel)
+{
+    widgetWithError = widgetWithLabel;
+    parentOfTheWidget = widgetWithLabel->parentWidget();
+
+}
+
+void ErrorDialog::evaluatePosition()
+{
+    //QRect widgetGeometry = widgetWithError->geometry();
+    QPoint mappingCoordinates = widgetWithError->mapTo(parentOfTheWidget,QPoint(0,0));
+    //QPoint mappingCoordinates = widgetWithError->mapToGlobal(widgetWithError->rect().topLeft());
+    //QPoint coordsOfWidget = widgetWithError->mapFromParent(QPoint(0,0));
+    //qDebug()<<coordsOfWidget;
+    //qDebug()<<mappingCoordinates;
+    errorLabelParent->move(mappingCoordinates + QPoint(-4, -4));
+}
+
+void ErrorDialog::setErrorType(ErrorDialog::ErrorType errorType)
+{
+    this->errorType = errorType;
+    if (errorType == ErrorType::INFO){
+        QPixmap pixInfo(":/images/Info.png");
+        errorLabel->setPixmap(pixInfo);
+    }
+    else if(errorType == ErrorType::SOFT_ERROR){
+        QPixmap pixSE(":/images/softError24_2.png");
+        errorLabel->setPixmap(pixSE);
+    }
+    else if(errorType == ErrorType::HARD_ERROR){
+        QPixmap pixHE(":/images/hardError24_2.png");
+        errorLabel->setPixmap(pixHE);
+    }
+    else{
+        QPixmap pixWTD(":/images/What_todo.png");
+        errorLabel->setPixmap(pixWTD);
+    }
+}
+
+
+
+void ErrorDialog::Info()
+{
+    setErrorType(ErrorType::INFO);
+}
+void ErrorDialog::SoftError()
+{
+    setErrorType(ErrorType::SOFT_ERROR);
+}
+void ErrorDialog::HardError()
+{
+    setErrorType(ErrorType::HARD_ERROR);
+}
+void ErrorDialog::What_todo()
+{
+    setErrorType(ErrorType::WHAT_TODO);
+}
+/*void ErrorDialog::createInfoDialog(QString &text, int typeOfEvent)
+{
+    errorStatementToDisplay = text;
+    errorSeverity = typeOfEvent;
+    ui->textError->setText(text);
+    if (typeOfEvent == 0) // soft error
+    {
+        QIcon soft(":/images/softError.png");
+        setWindowIcon(soft);
+        ui->tlacitko1->setEnabled(true);
+        ui->tlacitko1->setText(tr("OK"));
+    }
+    if (typeOfEvent == 1){ // hard error
+        QIcon hard(":/images/hardError.png");
+        setWindowIcon(hard);
+        ui->tlacitko1->setEnabled(true);
+        ui->tlacitko2->setEnabled(true);
+        ui->tlacitko1->setText(tr("Terminate"));
+        ui->tlacitko2->setText(tr("Continue"));
+    }
+}
+
+void ErrorDialog::on_tlacitko2_clicked()
+{
+    emit continueInCalculations();
     close();
 }
+
+void ErrorDialog::on_tlacitko1_clicked()
+{
+    if (errorSeverity == 0)
+        emit ok();
+    else
+        emit abort();
+    close();
+}*/
--- dialogy/errordialog.h
+++ dialogy/errordialog.h
@@ -2,24 +2,50 @@
 #define ERRORDIALOG_H
 
 #include <QDialog>
+#include <QString>
+#include <QWidget>
+#include <QLabel>
+#include <QGraphicsColorizeEffect>
 
-namespace Ui {
-class ErrorDialog;
-}
-
-class ErrorDialog : public QDialog
+class ErrorDialog : public QObject
 {
     Q_OBJECT
 
 public:
+    enum class ErrorType{
+        INFO,
+        SOFT_ERROR,
+        HARD_ERROR,
+        WHAT_TODO
+    };
     explicit ErrorDialog(QWidget *parent = nullptr);
     ~ErrorDialog();
+    //void createInfoDialog(QString& text,int typeOfEvent);
+    void evaluatePosition();
+    void analyseParents(QWidget* widgetWithLabel);
+    void setErrorType(ErrorDialog::ErrorType errorType);
+    void initialiseErrorLabel();
+    void Info();
+    void SoftError();
+    void HardError();
+    void What_todo();
 
 private slots:
-    void on_pushButton_clicked();
-
+    //void on_tlacitko2_clicked();
+    //void on_tlacitko1_clicked();
+signals:
+    /*void ok();
+    void abort();
+    void continueInCalculations();*/
 private:
-    Ui::ErrorDialog *ui;
+    QString errorStatementToDisplay;
+    int errorSeverity;
+    QLabel* errorLabel;
+    QGraphicsColorizeEffect* errorLabelEffect;
+    QWidget* parentOfTheWidget;
+    QWidget* widgetWithError;
+    QWidget* errorLabelParent;
+    ErrorType errorType = ErrorType::INFO;
 };
 
 #endif // ERRORDIALOG_H
