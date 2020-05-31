8a657d939611e48cb30a4c7df1f1da769c9e2a78
RaphCucul <prosserhonza@gmail.com>
2020-05-31 17:10:54 +0200

    Translatable error statemens

--- shared_staff/errors.h
+++ shared_staff/errors.h
@@ -2,40 +2,44 @@
 #define ERRORS_H
 #include <QJsonArray>
 #include <QString>
+#include <QObject>
 
 static QJsonArray hardErrorList = {
-    "No *.ini file found. Can't load program settings.", //0
-    "No *.json file with folders found. Can't load paths.", //1
-    "Empty folder path cannot be added.", //2
-    "Selected folder contains no *.avi files.", //3
-    "Can't save frangi parameters - the input must be non zero numbers.", //4
-    "Can't use frangi filter with zero or missing paramers.", //5
-    "Video could not be opened.", //6
-    "No file with Frangi parameters in this directory", //7
-    "Error occured when calculating ET of referencial image", //8
-    "Error occured when calculating ET of translated image", //9
-    "Error occured when preprocessing frames before full registration", //10
-    "Frames registration failed", //11
-    "Video writer could not be loaded.", //12
-    "Referencial frame could not be read", //13
-    "","","","","","","",""
+    QObject::tr("No *.ini file found. Can't load program settings."), //0
+    QObject::tr("No *.json file with folders found. Can't load paths."), //1
+    QObject::tr("Empty folder path cannot be added."), //2
+    QObject::tr("Selected folder contains no *.avi files."), //3
+    QObject::tr("Can't save frangi parameters - the input must be non zero numbers."), //4
+    QObject::tr("Can't use frangi filter with zero or missing paramers."), //5
+    QObject::tr("Video could not be opened."), //6
+    QObject::tr("No file with Frangi parameters in this directory"), //7
+    QObject::tr("Error occured when calculating ET of referencial image"), //8
+    QObject::tr("Error occured when calculating ET of translated image"), //9
+    QObject::tr("Error occured when preprocessing frames before full registration"), //10
+    QObject::tr("Frames registration failed"), //11
+    QObject::tr("Video writer could not be loaded."), //12
+    QObject::tr("Referencial frame could not be read"), //13
+    QObject::tr("Frangi anaylsis failed"), //14
 };
 
 static QJsonArray softErrorList = {
-    "*.ini file loaded successfully, but file folder path is empty.",//0
-    "*.ini file path must be similar with the application path.", //1
-    "An error occured when loading frangi json from this directory.", //2
-    "All processed videos are unprocessable.", //3
+    QObject::tr("*.ini file loaded successfully, but file folder path is empty."),//0
+    QObject::tr("*.ini file path must be similar with the application path."), //1
+    QObject::tr("An error occured when loading frangi json from this directory."), //2
+    QObject::tr("All processed videos are unprocessable."), //3
 };
 
 static QJsonArray infoList = {
-    "No *.dat file found for some chosen video. Use entropy and tennengrad analysis to add info about this video", //0
-    "More than one *.json file found. Can't decide, which one is correct.", //1
-    "Check the Frangi filter parameters if the values are suitable for the video(s).", //2
-    "No *.dat file found for this video.", //3
+    QObject::tr("No *.dat file found for some chosen video. Use entropy and tennengrad analysis to add info about this video"), //0
+    QObject::tr("More than one *.json file found. Can't decide, which one is correct."), //1
+    QObject::tr("Check the Frangi filter parameters if the values are suitable for the video(s)."), //2
+    QObject::tr("No *.dat file found for this video."), //3
+    QObject::tr("This frame is not official referential frame"), //4
 };
 
 static QJsonArray whatToDoList = {
-    "Problem with HDD counter occured. Please use the Settings option \"Add counter name\" to enable HDD usage plot", //0
+    QObject::tr("Problem with HDD counter occured. Please use the Settings option \"Add counter name\" to enable HDD usage plot"), //0
+    QObject::tr("Please write a referential frame number here."), //1
+    QObject::tr("Choose video and type referential frame number"), //2
 };
 #endif // ERRORS_H
