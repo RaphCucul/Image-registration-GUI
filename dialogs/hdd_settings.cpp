a311aacae27de52bbbe0927e3eef2ce97bcf38c6
RaphCucul <prosserhonza@gmail.com>
2020-11-16 19:21:43 +0100

    New HDD parameters extraction
    No more Powershell for Windows Registry processing. Plain C++.

--- dialogs/hdd_settings.cpp
+++ dialogs/hdd_settings.cpp
@@ -7,7 +7,6 @@
 #include <cstdlib>
 #include <unistd.h>
 #include <string>
-#include <windows.h>
 //#include "stdafx.h"
 
 #include "hdd_settings.h"
@@ -18,6 +17,7 @@
 #include <QDebug>
 #include <QFileDialog>
 #include <QMessageBox>
+#include <QRegularExpression>
 
 void ReplaceStringInPlace(std::string& subject, const std::string& search,
                           const std::string& replace) {
@@ -36,7 +36,6 @@ HDD_Settings::HDD_Settings(QWidget *parent) :
     connect(ui->buttonBox,SIGNAL(accepted()),this,SLOT(slot_accepted()));
     connect(ui->buttonBox,SIGNAL(rejected()),this,SLOT(slot_rejected()));
     connect(ui->runScript,SIGNAL(clicked()),this,SLOT(onRunScript()));
-    connect(ui->findPowershell,SIGNAL(clicked()),this,SLOT(onFindPowershell()));
 
     setLabelIcon(IconType::GREY);
     timer = new QTimer(this);
@@ -52,11 +51,8 @@ HDD_Settings::HDD_Settings(QWidget *parent) :
         ui->diodeLabel->setPixmap(QPixmap(":/images/greenDiode.png"));
         setLabelText(tr("Counter and parameter found"));
     }
-    powershell = GlobalSettings::getSettings()->getPowershellPath();
-    if (powershell.isEmpty())
-        ui->buttonBox->button(QDialogButtonBox::Ok)->setEnabled(false);
-    else
-        ui->powershellPath->setText(powershell);
+
+    _rE = new RegistryError();
 }
 
 HDD_Settings::~HDD_Settings()
@@ -96,7 +92,7 @@ void HDD_Settings::setLabelText(QString i_text){
 
 void HDD_Settings::onRunScript()
 {
-    std::string fileName = QString("/GetLocalCounterName.ps1").toLocal8Bit().constData();
+    /*std::string fileName = QString("/GetLocalCounterName.ps1").toLocal8Bit().constData();
 
     std::string strPath = GlobalSettings::getSettings()->getAppPath().toLocal8Bit().constData()+fileName;
     if(access(strPath.c_str(),0) == 0)
@@ -136,37 +132,110 @@ void HDD_Settings::onRunScript()
         setLabelIcon(IconType::RED);
         QString errorMessage = QString(tr("Unable to load the script"));
         setLabelText(errorMessage);
+    }*/
+    setLabelIcon(IconType::ORANGE);
+    QString message = QString(tr("Processing"));
+    setLabelText(message);
+    int disk = _rA.GetIndexForName("PhysicalDisk",_rE);
+    QString diskLocated = "";
+    if (disk != -1)
+        diskLocated = _rA.GetTranslationForName(disk,_rE);
+    int counter = _rA.GetIndexForName("% Disk Time",_rE);
+    QString counterLocated = "";
+    if (counter != -1)
+        counterLocated = _rA.GetTranslationForName(counter,_rE);
+    if (counter == -1 || disk == -1) {
+        setLabelIcon(IconType::RED);
+        QString errorMessage = QString(tr("An error occured when processing registry keys."));
+        setLabelText(errorMessage);
+    }
+    else {
+        setLabelIcon(IconType::GREEN);
+        //message = QString(tr("Processed."));
+        setLabelText(_rE->ErrorMessage());
+        ui->counterNameLE->setText(diskLocated);
+        ui->counterParameterLE->setText(counterLocated);
     }
 }
 
-void HDD_Settings::scanFolder(){
-    QStringList foundFiles;
-    int numberOfFiles=0;
-    analyseFileNames(GlobalSettings::getSettings()->getAppPath(),foundFiles,numberOfFiles,"txt");
-    if (foundFiles.contains("ScriptResult.txt")){
-        timer->stop();
-        QFile file(GlobalSettings::getSettings()->getAppPath()+"/ScriptResult.txt");
-        if (file.open(QIODevice::ReadOnly | QIODevice::Text)){
-            setLabelIcon(IconType::GREEN);
-            setLabelText(tr("Counter and parameter found"));
-            QTextStream in(&file);
-            while (!in.atEnd()) {
-                scriptResults.append(in.readLine());
-            }
-            ui->counterNameLE->setText(scriptResults.at(0));
-            ui->counterParameterLE->setText(scriptResults.at(1));
-            file.remove();
+int HDD_Settings::RegistryAccess::GetIndexForName(const QString i_name, RegistryError *o_rE) {
+
+    std::vector<std::wstring> counters = RegGetMultiString(
+        HKEY_LOCAL_MACHINE,
+        L"SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Perflib\\009",
+        L"Counter",
+        o_rE);
+    if (counters.size() != 0) {
+        for (unsigned int i=0; i < counters.size(); i++) {
+            if (counters.at(i) == i_name.toStdWString())
+                return i;
         }
     }
+    return -1;
 }
 
-void HDD_Settings::onFindPowershell() {
-    QString filepath = QFileDialog::getOpenFileName(this,tr("Find powershell.exe file"), QString(), "EXE (*.exe)");
-    if (!filepath.isEmpty()) {
-        if (filepath.mid(filepath.length()-4) == ".exe") {
-            GlobalSettings::getSettings()->setPowershellPath(filepath);
-            ui->powershellPath->setText(filepath);
-            ui->buttonBox->button(QDialogButtonBox::Ok)->setEnabled(true);
-        }
+QString HDD_Settings::RegistryAccess::GetTranslationForName(const int i_index, RegistryError *o_rE) {
+    std::vector<std::wstring> counters = RegGetMultiString(
+        HKEY_LOCAL_MACHINE,
+        L"SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Perflib\\CurrentLanguage",
+        L"Counter",
+        o_rE);
+    QString located = "";
+    if (counters.size() >= unsigned(i_index)) {
+        return QString::fromStdWString(counters.at(i_index));
     }
+    else
+        return located;
+}
+
+std::vector<std::wstring> HDD_Settings::RegistryAccess::RegGetMultiString(HKEY hKey,
+                                                                          const std::wstring& subKey,
+                                                                          const std::wstring& value,
+                                                                          RegistryError* o_rE)
+{
+    DWORD dataSize{};
+    std::vector<std::wstring> result = {};
+    LONG retCode = ::RegGetValue(
+        hKey,
+        subKey.c_str(),
+        value.c_str(),
+        RRF_RT_REG_MULTI_SZ,
+        nullptr,
+        nullptr,
+        &dataSize
+        );
+    if (retCode != ERROR_SUCCESS)
+    {
+        o_rE->setData("Cannot read multi-string from registry", retCode);
+        return result;
+    }
+    std::vector<wchar_t> data;
+    data.resize(dataSize / sizeof(wchar_t));
+    retCode = ::RegGetValue(
+        hKey,
+        subKey.c_str(),
+        value.c_str(),
+        RRF_RT_REG_MULTI_SZ,
+        nullptr,
+        &data[0],
+        &dataSize
+        );
+    if (retCode == ERROR_SUCCESS)
+    {
+        o_rE->setData("Cannot read multi-string from registry", retCode);
+        //return result;
+    }
+    data.resize( dataSize / sizeof(wchar_t) );
+    // Parse the double-NUL-terminated string into a vector<wstring>
+    const wchar_t* currStringPtr = &data[0];
+    while (*currStringPtr != L'\0')
+    {
+        // Current string is NUL-terminated, so get its length with wcslen
+        const size_t currStringLength = wcslen(currStringPtr);
+        // Add current string to result vector
+        result.push_back(std::wstring{ currStringPtr, currStringLength });
+        // Move to the next string
+        currStringPtr += currStringLength + 1;
+    }
+    return result;
 }
--- dialogs/hdd_settings.h
+++ dialogs/hdd_settings.h
@@ -3,6 +3,7 @@
 
 #include <QDialog>
 #include <QTimer>
+#include <windows.h>
 
 namespace Ui {
 class HDD_Settings;
@@ -48,19 +49,73 @@ private slots:
      */
     void onRunScript();
 
-    /**
-     * @brief Scans the program folder where the ouput of the powershell script is created when the process ends successfully.
-     * The interval of scanning is 1 second.
-     */
-    void scanFolder();
-
-    /**
-     * @brief Opens a file selection dialog to allow a user look for powershell.exe file.
-     */
-    void onFindPowershell();
-
 private:
     Ui::HDD_Settings *ui;
+
+    /**
+     * @class RegistryError
+     * @brief The RegistryError class stores error message created during Windows Registry analysis.
+     */
+    class RegistryError
+        : public std::runtime_error
+    {
+    public:
+        RegistryError() : runtime_error("Error when processing Windows Regitry."){};
+        /**
+         * @brief Saves data into RegistryError class object.
+         * @param[in] message - text of the message
+         * @param[in] errorCode - code representing Windows error
+         */
+        void setData(const char* message, LONG errorCode);
+        /**
+         * @brief Provides stored data from RegistryError class object.
+         * @return Complete string, contains error message and code
+         */
+        QString ErrorMessage() const noexcept
+        {
+            return QString("Error: %1 %2").arg(QString::fromLatin1(m_errorMessage)).arg(m_errorCode);
+        }
+    private:
+        LONG m_errorCode;
+        const char* m_errorMessage;
+    };
+
+    /**
+     * @class RegistryAccess
+     * @brief The RegistryAccess class object accesses Windows Registry, analyses specific keys and returns
+     * obtained information.
+     */
+    class RegistryAccess
+    {
+    public:
+        /**
+         * @brief Analyses Windows Registry key containing English original names for parameters. Index of the given
+         * requested parameter is returned.
+         * @param i_name - requested parameter name
+         * @param o_rE - RegistryError class object
+         * @return Index of the requested parameter in the complete list of parameters
+         */
+        int GetIndexForName(const QString i_name, RegistryError *o_rE);
+        /**
+         * @brief Creates vector of wstring objects from the REG_MULTI_SZ.
+         * @param hKey - main HKEY category
+         * @param subKey - complete path to the specific REG_MULTI_SZ
+         * @param value - name of the REG_MULTI_SZ parameter
+         * @param o_rE - RegistryError class object
+         * @return Obtained vector of wstring objects
+         */
+        std::vector<std::wstring> RegGetMultiString(HKEY hKey, const std::wstring& subKey, const std::wstring& value, RegistryError *o_rE);
+        /**
+         * @brief Analyses Windows Registry key containing localised parameters names. Given index is used to extract specific parameter
+         * name.
+         * @param i_index - numeric index of the requested parameter
+         * @param o_rE - RegistryError class object
+         * @return Localised name of the requested parameter
+         */
+        QString GetTranslationForName(const int i_index, RegistryError* o_rE);
+    private:
+    };
+
     /**
      * @brief Loads the icon of the corresponding icon type to the dialogue.
      * @param i_type
@@ -74,7 +129,8 @@ private:
 
     QTimer* timer = nullptr;
 
-    QStringList scriptResults;
+    RegistryAccess _rA;
+    RegistryError* _rE = nullptr;
 };
 
 #endif // HDD_SETTINGS_H
