b3051f18a27fe93cb8e9e8f246d30492aea23d82
RaphCucul <prosserhonza@gmail.com>
2020-12-29 17:14:02 +0100

    Comment section update

--- dialogs/singlevideoet.h
+++ dialogs/singlevideoet.h
@@ -17,6 +17,14 @@ namespace Ui {
 class SingleVideoET;
 }
 
+/**
+ * @class SingleVideoET
+ * @brief The SingleVideoET class is derived from the ETanalysisParent class and provides the option to select one video
+ * for E+T analysis and start the analysis with chosen parameters.
+ *
+ * Unlike in the MultipleVideoET, this class enables to edit the name of the video to load it quickly. If the selected name
+ * refers to a loadable video, it is automatically loaded and a user can immediately continue to work.
+ */
 class SingleVideoET : public ETanalysisParent
 {
     Q_OBJECT
@@ -25,102 +33,140 @@ public:
     virtual ~SingleVideoET();
 
 private slots:
+    /**
+     * @brief Opens QFileDialog so a user can choose a video for analysis.
+     *
+     * The function also checks if *.dat file for chosen video exists and tries to load info about cutouts. It also helps to
+     * check if a referential frame is known.
+     * @sa processFilePath(QString i_wholePaht, QString& o_folder, QString& o_onlyFilename, QString& o_suffix)
+     * @sa evaluateVideoImageInput(QString i_path)
+     */
     void on_chooseVideoPB_clicked();
+    /**
+     * @brief Called when a user changes the name of the video in the QLineEdit widget.
+     * @param arg1 - new video name
+     */
     void on_chosenVideoLE_textChanged(const QString &arg1);
+    /**
+     * @brief Starts E+T analysis.
+     */
     void on_calculateET_clicked();
+    /**
+     * @brief Displays computed results.
+     */
     void on_showGraphET_clicked();
 
     /**
      * @brief Display the name of actually processed video.
-     * @param index
+     * @param index - index of the video in the list
      */
     void newVideoProcessed(int index);
 
     /**
      * @brief Display the name of actually processed calculation.
-     * @param method
+     * @param method - informs about the progress
      */
     void movedToMethod(int method);
 
     /**
-     * @brief When one of thread is unexpectedly terminated, this function is called to raise an error message
-     * and to terminate all calculations.
-     * @param message
-     * @param threadNumber
-     * @param errorType
+     * @brief When one of thread is unexpectedly terminated, this function is called to terminate all calculations.
+     * @param videoIndex - unused
+     * @param errorType - "hardError" expected
      */
     void onUnexpectedTermination(int videoIndex, QString errorType);
 
     /**
-     * @brief Function calls protected done() function and another thread is initiated.
+     * @brief Calls protected done() function and another thread is initiated.
      */
     void onDone(int thread);
 
     /**
-     * @brief on_savePB_clicked
+     * @brief Saves complete calculated information.
      */
     void on_savePB_clicked();
 
     /**
-     * @brief onSaveFromGraphET
-     * @param i_videoName
+     * @brief Handles the command to save data if it comes from the GraphET class object.
+     * @param i_videoName - video name to identify data to be saved
+     * @param i_object - JSON object with information changed in the GraphET class object
      */
     void onSaveFromGraphET(QString i_videoName, QJsonObject i_object);
 
     /**
-     * @brief on_areaMaximum_editingFinished
+     * @brief When editing of "areaMaximum" parameter is finished, the function checks corectness.
+     * @sa checkValuesPass()
      */
     void on_areaMaximum_editingFinished();
 
     /**
-     * @brief on_rotationAngle_editingFinished
+     * @brief When editing of "rotationAngle" parameter is finished, the function checks corectness.
+     * @sa checkValuesPass()
      */
     void on_rotationAngle_editingFinished();
 
     /**
-     * @brief on_iterationCount_editingFinished
+     * @brief When editing of "iterationCount" parameter is finished, the function checks corectness.
+     * @sa checkValuesPass()
      */
     void on_iterationCount_editingFinished();
 
     /**
-     * @brief Function enables the main push button if all parameters - area, iteration and angle - are set
+     * @brief Enables the main push button if all parameters - areaMaximum, iterationCount and rotationAngle - are set
      * correctly.
+     * @sa on_areaMaximum_editingFinished()
+     * @sa on_rotationAngle_editingFinished()
+     * @sa on_iterationCount_editingFinished()
      */
     void evaluateCorrectValues();
 
     /**
-     * @brief Function creates a new dialog window with the referencial image as a content - clicking into the image
-     * marks new upper/lower or left/right border and corresponding extra cutout is generated later in algorithm.
+     * @brief Creates a new ClickImageEvent class object so a user can mark standard/extra cutout.
      */
     void showDialog();
 
     /**
-     * @brief Function deactivates specific widgets when the calculations start.
+     * @brief Disables specific widgets when the E+T analysis starts.
      */
     void disableWidgets();
 
     /**
-     * @brief Function re-enables specific widgets when the calculations stop.
+     * @brief Enables specific widgets when the E+T analysis stop.
      */
     void enableWidgets();
 
     /**
-     * @brief The function checks, if the path to the video folder exists. If so, the first video from the list of files
-     * found in the folder is placed into the line edit.
+     * @brief Checks if the path to the video folder exists.
+     *
+     * If so, the first video from the list of files
+     * found in the folder is placed into the QLineEdit widget.
      */
     void checkPaths();
 signals:
     /**
      * @brief If the value is added and if it is correct, the signal is emitted and evaluateCorrectValues
      * slot responds.
+     * @sa evaluateCorrectValues()
      */
     void checkValuesPass();
 private:
+    /**
+     * @brief Checks if input video or images can be loaded for next processing step.
+     * @param i_path
+     */
+    bool evaluateVideoImageInput(QString i_path);
+
+    /**
+     * @brief Checks if userly selected cutout fulfils all necessary criteria to be accepted.
+     *
+     * Basically, the modified cutout cannot be empty and its width and height must be greated than zero.
+     */
+    void checkSelectedCutout();
+
     Ui::SingleVideoET *ui;
     int formerIndex = 0;
     bool runStatus = true;
     QHash<QWidget*,ErrorDialog*> localErrorDialogHandling;
-    bool canProceed;
+    bool canProceed,standardLoaded=false,extraLoaded=false;
     QMap<QString,bool> ETthresholdsFound;
     cutoutType selectedCutout = cutoutType::NO_CUTOUT;
 };
